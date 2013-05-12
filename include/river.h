afaca9fb94e1a54f5a749c4f927419c565cf9a2c
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-05-12 18:22:14 -0300

    Adding interface 'river.h' to control the river animation

---
+++ include/river.h
@@ -0,0 +1,14 @@
+#ifndef H_RIVER_DEFINED
+#define H_RIVER_DEFINED
+
+/* Funções para configuração de parâmetros */
+void river_config_flux        (int);
+void river_config_size        (int, int); /* largura/altura */
+void river_config_margins     (int, int); /* esquerda/direita */
+
+/* Funções para controle de animação */
+void river_animation_generate (int);      /* semente */
+void river_animation_iterate  ();
+void river_animation_finish   (); 
+
+#endif
